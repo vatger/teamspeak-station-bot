2f03137d91f276d2298d1edcfde0319e9a2d7f0e
Nikolas Görlitz <48025581+ngoerlitz@users.noreply.github.com>
2023-08-22 16:38:09 +0200

    Update dataFeed.service.ts

--- src/services/dataFeed.service.ts
+++ src/services/dataFeed.service.ts
@@ -6,16 +6,16 @@ async function getControllersFromDatafeed() {
     const res = await axios.get(config().vatsimDatafeedUrl);
     const datafeed = res.data;
 
+    if (datafeed == null || datafeed.data?.failed == false) {
+        throw new Error("Datafeed is down, thanks vatsim");
+    }
+    
     // The cached datafeed has an extra nested "data" object (see https://github.com/vatger/datafeed-cache/wiki)
     let controllers = datafeed?.data?.controllers ?? [];
 
     controllers = controllers.filter((controller: DatafeedController) => {
         return (controller.facility != 0 && controller.frequency != "199.998" && (controller.callsign.startsWith("ED") || controller.callsign.startsWith("ET")));
     });
-
-    if (datafeed == null || datafeed.data?.failed == false) {
-        throw new Error("Datafeed is down, thanks vatsim");
-    }
 
     return controllers;
 }
