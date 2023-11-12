4d9a30a142c834267533a07b0c76b4142906f422
Nikolas Görlitz <git@ngoerlitz.de>
2023-11-12 13:41:53 +0100

    Update internal homepage API

--- src/services/vatger.service.ts
+++ src/services/vatger.service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import config from "../config";
 
 async function getTeamspeakDbIdFromCID(cid: number) {
-    return axios.get(`http://hp.vatsim-germany.org/api/teamspeak/${cid}`, {
+    return axios.get(`http://vatsim-germany.org/api/teamspeak/${cid}`, {
         headers: {
             Authorization: `Token ${config().apiToken}`,
         },
